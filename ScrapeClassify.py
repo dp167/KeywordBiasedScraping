225fe6bc53711ccf971bfa23e908f51a239196ce
David Poole <42785393+dp167@users.noreply.github.com>
2020-03-05 16:53:13 -0800

    update web scraper
    add more news outlets and fix formatting

--- ScrapeClassify.py
+++ ScrapeClassify.py
@@ -1,6 +1,7 @@
 #from google import google
 import google
 from googlesearch import search
+import plotly.graph_objects as go
 import re
 from textblob import TextBlob
 import texttable as tt
@@ -15,13 +16,6 @@ def search_web( query):
     middle = 0
     middleleft = 0
     farleft = 0
-    #regex = re.compile('fox')A
-    fox = "fox"
-    dailywire = "dailywire"
-    newyorkpost = "newyorkpost"
-    washingtontimes = "washingtontimes"
-    infowars = "infowars"
-
 
     search_results = []
     k = 0
@@ -34,11 +28,22 @@ def search_web( query):
         #print(i)
     for j in search_results:
         print(j)
-        if ((j.find(fox) > -1) or (j.find(dailywire)>-1) or (j.find(newyorkpost) > -1 ) or (j.find(washingtontimes)> -1) or j.find(infowars)>-1):
+        if ((j.find("fox") > -1) or (j.find("dailywire")>-1) or (j.find("newyorkpost") > -1 ) or (j.find("washingtontimes")> -1) or j.find("infowars")>-1):
             farright = farright +1
         if(j.find("reuters")>-1)or j.find("bloomberg") >-1 or j.find("abc")>-1 or j.find("usatoday")>-1:
             middle = middle + 1
+        if(j.find("npr")>-1)or (j.find("bbc")>-1)or (j.find("nbc")>-1) or (j.find("washingtonpost")>-1) or (j.find("politico")>-1)or (j.find("guardian")>-1)or (j.find("newyorktimes")>-1)or (j.find("newyorker")>-1)or (j.find("")>-1):
+           middleleft = middleleft + 1
+        if(j.find("huffington")>-1)or (j.find("buzz")>-1)or (j.find("msnb")>-1) or (j.find("slate")>-1):
+            farleft = farleft+1
     print(farright)
     print(middle)
+    print(farleft)
 
-search_web("trump")
+    fig = go.Figure(
+        data=[go.Bar(y=[farleft, middle, farright])],
+        layout_title_text="A Figure Displaying political bias from left to right"
+    )
+    fig.show(renderer="png")
+
+search_web("corona")
